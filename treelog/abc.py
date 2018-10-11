41694fb288f317d55067c4b96d34602971910220
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2018-10-11 19:42:07 +0200

    add treelog module

---
+++ treelog/abc.py
@@ -0,0 +1,151 @@
+# Copyright (c) 2018 Evalf
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import abc, functools, warnings
+from . import _io
+
+class Closing(abc.ABC):
+  '''Base class for enterable objects that close upon exit.
+
+  A subclass should define the :meth:`close` method for cleanup that returns a
+  true value if the object was found to be open.'''
+
+  @abc.abstractmethod
+  def close(self):
+    raise NotImplementedError
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, *args):
+    self.close()
+
+  def __del__(self):
+    if self.close():
+      warnings.warn('unclosed object {!r}'.format(self), ResourceWarning)
+
+class ClosingGenerator(Closing):
+  '''Enterable generator that close upon exit.
+
+  Generator wrapper that tracks whether the generator was exhausted or closed
+  manually to enable a destruction warning.'''
+
+  @classmethod
+  def compose(cls, f):
+    return functools.wraps(f)(lambda *args, **kwargs: cls(f(*args, **kwargs)))
+
+  def __init__(self, gen):
+    self._gen = gen
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    if not self._gen:
+      raise StopIteration
+    try:
+      return next(self._gen)
+    except StopIteration:
+      self._gen = None
+      raise
+
+  def close(self):
+    if self._gen:
+      self._gen.close()
+      self._gen = None
+      return True
+
+class Log(abc.ABC):
+  '''Abstract base class for log objects.
+
+  A subclass must define a :meth:`context` method that handles a context
+  change, a :meth:`write` method that logs a message, and an :meth:`open`
+  method that returns a file context.'''
+
+  @abc.abstractmethod
+  def context(self, title):
+    raise NotImplementedError
+
+  @abc.abstractmethod
+  def write(self, text, level):
+    raise NotImplementedError
+
+  @abc.abstractmethod
+  def open(self, filename, mode, level, id):
+    raise NotImplementedError
+
+  @ClosingGenerator.compose
+  def iter(self, title, iterable, length=None):
+    if length is None:
+      try:
+        length = len(iterable)
+      except:
+        pass
+    for i, val in enumerate(iterable):
+      text = '{} {}'.format(title, i)
+      if length:
+        text += ' ({:.0f}%)'.format(100 * (i+.5) / length)
+      with self.context(text):
+        yield val
+
+  def _factory(level):
+
+    def print(self, *args, sep=' '):
+      '''Write message to log.
+
+      Args
+      ----
+      *args : tuple of :class:`str`
+          Values to be printed to the log.
+      sep : :class:`str`
+          String inserted between values, default a space.
+      '''
+      self.write(sep.join(map(str, args)), level)
+
+    def file(self, name, mode, *, id=None):
+      '''Open file in logger-controlled directory.
+
+      Args
+      ----
+      filename : :class:`str`
+      mode : :class:`str`
+          Should be either ``'w'`` (text) or ``'wb'`` (binary data).
+      id :
+          Bytes identifier that can be used to decide a priori that a file has
+          already been constructed. Default: None.
+      '''
+      return self.open(name, mode, level, id)
+
+    name = ['debug', 'info', 'user', 'warning', 'error'][level]
+    print.__name__ = name
+    print.__qualname__ = 'Log.' + name
+    file.__name__ = name + 'file'
+    file.__qualname__ = 'Log.' + name + 'file'
+    return print, file
+
+  debug,   debugfile   = _factory(0)
+  info,    infofile    = _factory(1)
+  user,    userfile    = _factory(2)
+  warning, warningfile = _factory(3)
+  error,   errorfile   = _factory(4)
+
+  del _factory
+
+# vim:sw=2:sts=2:et
---
+++ treelog/ctx.py
@@ -0,0 +1,131 @@
+# Copyright (c) 2018 Evalf
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import contextlib, logging, sys
+from . import abc, _io
+
+class ContextLog(abc.Log):
+  '''Base class for loggers that keep track of the current list of contexts.
+
+  The base class implements :meth:`context` and :meth:`open` which keep the
+  attribute :attr:`_context` up-to-date.
+
+  .. attribute:: _context
+
+     A :class:`list` of contexts (:class:`str`\\s) that are currently active.
+  '''
+
+  def __init__(self):
+    self._context = []
+
+  @contextlib.contextmanager
+  def context(self, title):
+    try:
+      depth = len(self._context)
+      self._context.append(title)
+      yield
+    finally:
+      self._context.pop()
+      if len(self._context) != depth:
+        self.warning('context ended out of order')
+
+  @contextlib.contextmanager
+  def open(self, filename, mode, level, id):
+    with self.context(filename), _io.devnull(filename) as f:
+      yield f
+    self.write(filename, level=level)
+
+class StdoutLog(ContextLog):
+  '''Output plain text to stream.'''
+
+  def write(self, text, level):
+    print(*self._context, text, sep=' > ')
+
+class RichOutputLog(ContextLog):
+  '''Output rich (colored,unicode) text to stream.'''
+
+  def __init__(self, interval=.1):
+    import _thread
+    super().__init__()
+    self._sleep = _thread.allocate_lock() # to be acquired by thread, released by self
+    self._state = [False, False] # mutable shared state: [killed, timing]
+    _thread.start_new_thread(self._context_thread, (self._context, self._sleep, self._state, interval))
+
+  @staticmethod
+  def _context_thread(context, sleep, state, interval): # pragma: no cover (tested via stdout)
+    try:
+      while not state[0]: # object is alive
+        if not sleep.acquire(timeout=interval if state[1] else -1): # timed out
+          sys.stdout.write('\033[K\033[1;30m' + ' · '.join(context) + '\033[0m\r' if context else '\033[K\r')
+          state[1] = False # sleep until further notice
+    except Exception as e:
+      sys.stdout.write('\033[Kcontext thread died unexpectedly: {}\n'.format(e))
+
+  def _wakeup_thread(self):
+    if self._sleep.locked():
+      self._sleep.release()
+
+  @contextlib.contextmanager
+  def context(self, title):
+    try:
+      with super().context(title):
+        if not self._state[1]:
+          self._state[1] = True
+          self._wakeup_thread() # start countdown
+        yield
+    finally:
+      if not self._state[1]:
+        self._state[1] = True
+        self._wakeup_thread() # start countdown
+
+  def write(self, text, level):
+    line = '\033[K' # clear line
+    if self._context:
+      line += '\033[1;30m' + ' · '.join(self._context) + ' · ' # context in gray
+    if level == 4: # error
+      line += '\033[1;31m' # bold red
+    elif level == 3: # warning
+      line += '\033[0;31m' # red
+    elif level == 2: # user
+      line += '\033[1;34m' # bold blue
+    elif self._context:
+      line += '\033[0m' # reset color
+    line += text
+    line += '\033[0m\n' # reset and newline
+    sys.stdout.write(line)
+    self._wakeup_thread() # start/restart countdown
+
+  def __del__(self):
+    if hasattr(self, '_state'):
+      self._state[0] = True # order matters: first signal that the thread should stop
+      self._state[1] = True # then make sure that it doesn't accidentally fall asleep
+      self._wakeup_thread()
+
+class LoggingLog(ContextLog):
+  '''Log to Python's built-in logging facility.'''
+
+  _levels = logging.DEBUG, logging.INFO, 25, logging.WARNING, logging.ERROR
+
+  def __init__(self, name='nutils'):
+    self._logger = logging.getLogger(name)
+    super().__init__()
+
+  def write(self, text, level):
+    self._logger.log(self._levels[level], ' > '.join((*self._context, text)))
