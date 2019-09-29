29b0d19f86379564b120051cdc55da065bb6055c
Joost van Zwieten <joostvanzwieten@evalf.com>
2019-09-29 21:23:28 +0200

    support building treelog (partially) with mypyc
    Set `TREELOG_USE_MYPYC=1` before building or installing the package.

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ import os, re
 with open(os.path.join('treelog', '__init__.py')) as f:
   version = next(filter(None, map(re.compile("^version = '([a-zA-Z0-9.]+)'$").match, f))).group(1)
 
+if os.getenv('TREELOG_USE_MYPYC', None) == '1':
+  from mypyc.build import mypycify
+  import pathlib
+  ext_modules = mypycify([str(p) for p in pathlib.Path('treelog').glob('*.py') if p.name not in {'__init__.py', 'proto.py'}])
+else:
+  ext_modules = []
+
 setup(
   name = 'treelog',
   version = version,
@@ -13,6 +20,7 @@ setup(
   url = 'https://github.com/evalf/treelog',
   packages = ['treelog'],
   package_data = {'treelog': ['py.typed']},
+  ext_modules=ext_modules,
   license = 'MIT',
   python_requires = '>=3.5',
   install_requires = ['typing_extensions'],
