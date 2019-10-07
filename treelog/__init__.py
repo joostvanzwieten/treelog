a8ad1e1f28186dca4b0df5b240136226b2d3e457
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2019-10-07 15:58:26 +0200

    bump version to v1.0b6

--- treelog/__init__.py
+++ treelog/__init__.py
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-version = '1.0b5'
+version = '1.0b6'
 
 import sys, functools, contextlib, typing, typing_extensions
 
